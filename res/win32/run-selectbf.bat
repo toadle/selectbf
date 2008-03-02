94bb3c8e72a8258f4ea2b07dafd4a096bc611b53
tadler <tadler>
2008-03-02 14:24:44 +0000

    Corrected issues in the launch-files

--- res/win32/run-selectbf.bat
+++ res/win32/run-selectbf.bat
@@ -1,1 +1,1 @@
-java -cp .\selectbf.jar;..\lib\commons-net-1.3.0.jar;..\lib\mysql-connector-java-3.1.7-bin.jar;..\lib\jdom.jar; org.selectbf.SelectBf
+java -Xmx256m -Xms256m -cp .\selectbf.jar;..\lib\commons-net-1.3.0.jar;..\lib\mysql-connector-java-5.0.5-bin.jar;..\lib\jdom.jar;..\lib\log4j-1.2.9.jar; org.selectbf.SelectBf
